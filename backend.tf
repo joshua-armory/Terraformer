5f6706cc02f9707854c6a1be10ed48bd635e88c5
joshua-armory <99360899+joshua-armory@users.noreply.github.com>
2022-05-08 07:55:40 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -5,7 +5,7 @@ terraform {
         token = "pJdqHvJzVJIevw.atlasv1.uye3qcfL2zz1pVg4TQ72ZKvg0LhBRGVRnqkfO9ce06YrGs9Ru8L8j2psarSkuchcpWs"
 
         workspaces {
-            name = "terraformer"
+            name = "spinnaker"
         }
     }
 }
